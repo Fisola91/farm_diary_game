2eb9d43ce9385e38c9c4b286c8b62d69cd0a1868
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-17 10:22:48 +0200

    modified the start_game method

--- lib/interface.rb
+++ lib/interface.rb
@@ -7,18 +7,18 @@ def start_game
 
   action = " "
   until action == "quit"
-    puts "\n\nPick an action: [corn|rice|quit]"
+    puts "\nPick an action: [corn|rice|quit]"
     print "> "
-    action = gets.chomp
+    action = gets.chomp!
     case action
     when "corn"
       puts "Let's plant corn crops!"
     when "rice"
       puts "Let's plant rice crops"
     when "quit"
-      puts "see you next time"
+      puts "See you next time"
     else
-      puts "i don't know what you mean"
+      puts "I don't know what you mean"
     end
   end
 end
